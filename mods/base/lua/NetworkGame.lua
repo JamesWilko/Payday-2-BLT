5d6b29f6b503e6eb57d3f73eb2e4b2f3862dbdc0
Simon <great.big.bushy.beard.gbbb@gmail.com>
2015-02-18 22:21:13 +0000

    Changed the naming of the Hooks to be more inline with the rest of the project

--- mods/base/lua/NetworkGame.lua
+++ mods/base/lua/NetworkGame.lua
@@ -1,30 +1,30 @@
 CloneClass(NetworkGame)
 
-Hooks:Register("NetworkGame_lobby_entered")
+Hooks:Register("NetworkGameOnEnteredLobby")
 function NetworkGame.on_entered_lobby(self)
 	self.orig.on_entered_lobby(self)
 	local local_peer = managers.network:session():local_peer()
 	local id = local_peer:id()
-	Hooks:Call("NetworkGame_lobby_entered", local_peer, id)
+	Hooks:Call("NetworkGameOnEnteredLobby", local_peer, id)
 end
 
-Hooks:Register("NetworkGame_joined_game")
+Hooks:Register("NetworkGameOnGameJoined")
 function NetworkGame.on_game_joined(self)
 	self.orig.on_game_joined(self)
 	local local_peer = managers.network:session():local_peer()
 	local id = local_peer:id()
-	Hooks:Call("NetworkGame_joined_game", local_peer, id)
+	Hooks:Call("NetworkGameOnGameJoined", local_peer, id)
 end
 
-Hooks:Register("NetworkGame_peer_entered_lobby")
+Hooks:Register("NetworkGameOnPeerEnteredLobby")
 function NetworkGame.on_peer_entered_lobby(self, peer_id)
 	self.orig.on_peer_entered_lobby(self, peer_id)
 	local peer = managers.network:session():peer(peer_id)
-	Hooks:Call("NetworkGame_peer_entered_lobby", peer, peer_id)
+	Hooks:Call("NetworkGameOnPeerEnteredLobby", peer, peer_id)
 end
 
-Hooks:Register("NetworkGame_peer_added")
+Hooks:Register("NetworkGameOnPeerAdded")
 function NetworkGame.on_peer_added(self, peer, peer_id)
 	self.orig.on_peer_added(self, peer, peer_id)
-	Hooks:Call("NetworkGame_peer_added", peer, peer_id)
+	Hooks:Call("NetworkGameOnPeerAdded", peer, peer_id)
 end
