a282ee341dd42aa9ea537e55360a6c476ff4e378
James Wilkinson <jw@jameswilko.com>
2015-03-28 23:02:11 +1100

    Return hooks can return multiple values, up to 32

--- mods/base/req/hooks.lua
+++ mods/base/req/hooks.lua
@@ -128,9 +128,10 @@ function Hooks:ReturnCall( key, ... )
 		if v then
 			if type(v.func) == "function" then
 
-				local r = v.func( ... )
-				if r ~= nil then
-					return r
+				-- Holy hell would you look at this shit
+				local r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32 = v.func( ... )
+				if r1 ~= nil then
+					return r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32
 				end
 
 			end
