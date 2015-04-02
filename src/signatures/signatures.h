6b2e42494678ade6651dd85313730261b2dc3fcd
Oliver <olipro@uptheinter.net>
2015-04-02 01:15:27 -0700

    Add BLT runtime detection support for client/standalone mode.
    The BLT hook will determine at runtime whether it is running under the
    existing hook and behave appropriately according to the situation.
    
    Namely; if running as a standalone, signature lookups will be performed
    in order to populate function pointers. Otherwise, it will simply use
    GetProcAddress to fetch function addresses from the existing hook.

--- src/signatures/signatures.h
+++ src/signatures/signatures.h
@@ -4,15 +4,22 @@
 #include <string>
 #include <vector>
 
+#define LUAFUNC_PTR(name, retn, ...) \
+	retn(*name)(__VA_ARGS__)
+
+#define HOOK_PTR(name, calltype, retn, ...) \
+	retn(calltype *name)(__VA_ARGS__)
+
+#define RESOLVE(handle, name) \
+	name = (decltype(name))GetProcAddress(handle, #name)
+
 #define CREATE_CALLABLE_SIGNATURE(name, retn, signature, mask, offset, ...) \
-	typedef retn(*name ## ptr)(__VA_ARGS__); \
-	name ## ptr name = NULL; \
-	SignatureSearch name ## search(&name, signature, mask, offset);
+	void** name##Ptr = (void**)&name; \
+	SignatureSearch(name##Ptr, signature, mask, offset);
 
 #define CREATE_CALLABLE_CLASS_SIGNATURE(name, retn, signature, mask, offset, ...) \
-	typedef retn(__thiscall *name ## ptr)(void*, __VA_ARGS__); \
-	name ## ptr name = NULL; \
-	SignatureSearch name ## search(&name, signature, mask, offset);
+	void** name##Ptr = (void**)&name; \
+	SignatureSearch(name##Ptr, signature, mask, offset);
 
 #define CREATE_LUA_FUNCTION(lua_func, name) \
 	lua_pushcclosure(L, lua_func, 0); \
@@ -22,12 +29,12 @@ struct SignatureF {
 	const char* signature;
 	const char* mask;
 	int offset;
-	void* address;
+	void** address;
 };
 
 class SignatureSearch {
 public:
-	SignatureSearch(void* address, const char* signature, const char* mask, int offset);
+	SignatureSearch(void** address, const char* signature, const char* mask, int offset);
 	static void Search();
 };
 
