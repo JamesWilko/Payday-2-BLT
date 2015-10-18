59ef5a5de55538499d9d51907e075af92adbb74d
BonusPlay <bonusplay@taw.net>
2015-10-18 15:48:58 +0200

    Push #1

--- mods/base/lua/GameSetup.lua
+++
@@ -1,8 +0,0 @@
-
-CloneClass( GameSetup )
-
-Hooks:RegisterHook("GameSetupUpdate")
-function GameSetup.update(this, t, dt)
-	Hooks:Call("GameSetupUpdate", t, dt)
-	return this.orig.update(this, t, dt)
-end
--- mods/base/lua/NetworkManager.lua
+++
@@ -1,7 +0,0 @@
-CloneClass(NetworkManager)
-
-Hooks:Register("NetworkManagerOnPeerAdded")
-function NetworkManager.on_peer_added(self, peer, peer_id)
-	self.orig.on_peer_added(self, peer, peer_id)
-	Hooks:Call("NetworkManagerOnPeerAdded", peer, peer_id)
-end
